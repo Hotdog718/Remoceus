0dc86dd0662eb0b71d22eab79e836e54eb425516
Alexander <abertholf7626@floridapoly.edu>
2019-11-02 15:26:29 -0400

    Update TakeBadges.js
    - Moved gym leader check to errors.js

--- Commands/Gyms/TakeBadge.js
+++ Commands/Gyms/TakeBadge.js
@@ -14,26 +14,7 @@ module.exports = {
 		if(!pUser) return errors.noUser(message);
 		if(!type) return errors.noType(message);
 
-		if(((type.toLowerCase() === "normal" && message.member.roles.find(role => role.name === "Normal Gym Leader"))
-		|| (type.toLowerCase() === "fire" && message.member.roles.find(role => role.name === "Fire Gym Leader"))
-		|| (type.toLowerCase() === "water" && message.member.roles.find(role => role.name === "Water Gym Leader"))
-		|| (type.toLowerCase() === "Grass" && message.member.roles.find(role => role.name === "Grass Gym Leader"))
-		|| (type.toLowerCase() === "electric" && message.member.roles.find(role => role.name === "Electric Gym Leader"))
-		|| (type.toLowerCase() === "flying" && message.member.roles.find(role => role.name === "Flying Gym Leader"))
-		|| (type.toLowerCase() === "bug" && message.member.roles.find(role => role.name === "Bug Gym Leader"))
-		|| (type.toLowerCase() === "ghost" && message.member.roles.find(role => role.name === "Ghost Gym Leader"))
-		|| (type.toLowerCase() === "poison" && message.member.roles.find(role => role.name === "Poison Gym Leader"))
-		|| (type.toLowerCase() === "psychic" && message.member.roles.find(role => role.name === "Psychic Gym Leader"))
-		|| (type.toLowerCase() === "dragon" && message.member.roles.find(role => role.name === "Dragon Gym Leader"))
-		|| (type.toLowerCase() === "dark" && message.member.roles.find(role => role.name === "Dark Gym Leader"))
-		|| (type.toLowerCase() === "rock" && message.member.roles.find(role => role.name === "Rock Gym Leader"))
-		|| (type.toLowerCase() === "ground" && message.member.roles.find(role => role.name === "Ground Gym Leader"))
-		|| (type.toLowerCase() === "fairy" && message.member.roles.find(role => role.name === "Fairy Gym Leader"))
-		|| (type.toLowerCase() === "ice" && message.member.roles.find(role => role.name === "Ice Gym Leader"))
-		|| (type.toLowerCase() === "fighting" && message.member.roles.find(role => role.name === "Fighting Gym Leader"))
-		|| (type.toLowerCase() === "steel" && message.member.roles.find(role => role.name === "Steel Gym Leader")))
-		|| message.member.hasPermission("MANAGE_ROLES", true, true)){
-
+		if(client.errors.checkGyms(type, message.member, true)){
 			Badges.findOne({
 				userID: pUser.id,
 				serverID: message.guild.id
@@ -76,8 +57,7 @@ module.exports = {
 					badges.save().catch(err => console.log(err));
 				}
 			})
-		}
-		else {
+		}else {
 			return message.channel.send("oof.").then(msg => msg.delete(5000));
 		}
 	}
