fc243583ba2490920ce509e35f915f9cbf737c34
Alexander <abertholf7626@floridapoly.edu>
2019-11-02 12:23:43 -0400

    Update index.js
    -added type colors

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
+//@ts-check
+
 const { Client, Collection } = require("discord.js");
 const client = new Client({disableEveryone: true});
 const fs = require("fs");
 const botconfig = require("./botconfig.json")
 const errors = require("./Utils/Errors.js");
+const helper = require("./Utils/Helper.js")
+const typeColors = require("./Type Colors.json");
 
 const token = require("./token.json")
 const mongoose = require("mongoose")
@@ -15,6 +19,9 @@ client.config = botconfig;
 client.commands = new Collection();
 client.aliases = new Collection();
 client.errors = errors;
+client.gymTypes = ["bug", "dark", "dragon", "electric","fairy","fighting","fire","flying","ghost","grass","ground","ice","normal","poison","psychic","rock","steel","water"];
+client.helpers = helper;
+client.typeColors = typeColors;
 
 const Handlers = ["Commands.js", "Events.js"];
 Handlers.forEach((r) => {
