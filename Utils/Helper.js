08f1dca782111c96f3c7fb0f2dfcc60c4602ba15
Alexander <abertholf7626@floridapoly.edu>
2019-11-02 12:22:20 -0400

    Added Helper Files
    -Type Colors.json
    -Helper.js

---
+++ Utils/Helper.js
@@ -0,0 +1,17 @@
+
+
+module.exports = {
+	getTitleCase: (word) => {
+		//takes a string as an argument and returns a string (getTitleCase("hello") => returns "Hello")
+		if(word.length === 0) return word;
+		return `${word.substring(0,1).toUpperCase()}${word.substring(1).toLowerCase()}`;
+	}, 
+	rankLinks: {
+		"SS": "https://i.imgur.com/DQun29l.png",
+		"S": "https://i.imgur.com/tdHZnt4.png",
+		"A": "https://i.imgur.com/hCcIx73.png",
+		"B": "https://i.imgur.com/Q32a9Ys.png",
+		"C": "https://i.imgur.com/JFbzol2.png",
+		"D": "https://i.imgur.com/TSAzjav.png",
+	}
+}
