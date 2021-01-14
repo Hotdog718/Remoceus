8aca7bd6c45dcae1eb3e8e84ee7f9c373bef8283
Alexander <abertholf7626@floridapoly.edu>
2021-01-13 19:12:58 -0500

    Update Errors.js
    - Will now react to message with an emote if a function is called

--- Utils/Errors.js
+++ Utils/Errors.js
@@ -3,11 +3,14 @@
 module.exports = {
 	noUser: (message) => {
 		message.channel.send("No user found");
+		message.react('❌');
 	},
 	noPerms: (message, perm) => {
 		message.channel.send(`Missing Permission: ${perm}`);
+		message.react('❌');
 	},
 	noType: (message) => {
 		message.channel.send("Could not find gym type");
+		message.react('❌');
 	}
 }
