0f30a9335a0f72bbccc142ed70fb2a92e9612528
Alexander <abertholf7626@floridapoly.edu>
2021-01-14 11:44:26 -0500

    Added createListEmbed function and updated createMenuEmbed

--- Utils/Helper.js
+++ Utils/Helper.js
@@ -45,47 +45,111 @@ module.exports = {
 		let typeGymLeaders = guildMembers.filter(member => member.roles.cache.has(typeRole.id) && member.roles.cache.has(subRole.id))
 		return typeGymLeaders;
 	},
+	createListEmbed: (client, message, data, resultsPerPage, embedFunction) => {
+		let index = 0;
+		let maxPages = Math.ceil(data.length/resultsPerPage);
+
+		message.channel.send(embedFunction(client, message, data, resultsPerPage, index))
+		.then(msg => {
+			let reactions = ["⬅", "➡", "⏹"];
+			reactions.forEach(function(r, i){
+				setTimeout(function(){
+					msg.react(r);
+				}, i*800)
+			})
+
+			//set filter to only let only set reactions and message author to respond
+			const filter = (reaction, user) => {
+				return reactions.includes(reaction.emoji.name) && user.id === message.author.id;
+			}
+
+			//create reactionCollector
+			const collector = msg.createReactionCollector(filter, {});
+
+			collector.on('collect', async (reaction, user) => {
+				const userReactions = msg.reactions.cache.filter(reaction => reaction.users.cache.has(message.author.id));
+				try {
+					for (const reaction of userReactions.values()) {
+						await reaction.users.remove(message.author.id);
+					}
+				} catch (error) {
+					console.error('Failed to remove reactions.');
+				}
+				switch(reaction.emoji.name){
+					case '⬅':{
+						index = (index-1) < 0? maxPages-1 :index-1;
+						msg.edit(embedFunction(client, message, data, resultsPerPage, index));
+						break;
+					}
+					case '➡':{
+						index = (index+1)%maxPages;
+						msg.edit(embedFunction(client, message, data, resultsPerPage, index));
+						break;
+					}
+					case '⏹':{
+						collector.emit('end');
+						break;
+					}
+				}
+			})
+
+			collector.on('end', collected => {
+				msg.delete();
+			})
+		})
+		.catch(console.error);
+	},
 	createMenuEmbed: (client, message, data, embedFunction) => {
 		const { MessageCollector } = require('discord.js');
 		let index = 0;
 		let embed = embedFunction(client, index, data);
 		message.channel.send(embed).then(msg => {
+			let reactions = ["⬅", "➡", '⏹'];
+			reactions.forEach(function(r, i){
+				setTimeout(function(){
+					msg.react(r);
+				}, i*800)
+			})
+
 			//set filter to only let only set reactions and message author to respond
-      const filter = (m) => {
-        return m.author.id === message.author.id;
-      }
+			const filter = (reaction, user) => {
+				return reactions.includes(reaction.emoji.name) && user.id === message.author.id;
+			}
 
-			//create msgCollector
-      const collector = new MessageCollector(message.channel, filter, {idle: 60000});
+			//create Reaction Collector
+			const collector = msg.createReactionCollector(filter, {});
 
-			collector.on('collect', (m) => {
-        switch(m.content){
-          case '!back': {
-            if(m.deletable) m.delete();
-            index = (index-1) < 0 ? data.length-1 : index-1;
-            msg.edit(embedFunction(client, index, data)).catch(err => collector.stop());
-            break;
-          }
-          case '!next': {
-            if(m.deletable) m.delete();
-            index = (index+1)%data.length;
-            msg.edit(embedFunction(client, index, data)).catch(err => collector.stop());
-            break;
-          }
-          case '!stop': {
-						if(m.deletable) m.delete();
-            collector.stop("Manually Stopped");
-            break;
-          }
-					default: break;
-        }
-      })
-
-			collector.on('end', (collected, reason) => {
-				if(reason === "Manually Stopped" && msg.deletable){
-					msg.delete();
+			collector.on('collect', async(reaction, user) => {
+				const userReactions = msg.reactions.cache.filter(reaction => reaction.users.cache.has(message.author.id));
+				try {
+					for (const reaction of userReactions.values()) {
+						await reaction.users.remove(message.author.id);
+					}
+				} catch (error) {
+					console.error('Failed to remove reactions.');
 				}
-			});
+				switch(reaction.emoji.name){
+					case '⬅': {
+						index = (index-1) < 0 ? data.length-1 : index-1;
+						msg.edit(embedFunction(client, index, data)).catch(err => collector.stop());
+						break;
+					}
+					case '➡': {
+						index = (index+1)%data.length;
+						msg.edit(embedFunction(client, index, data)).catch(err => collector.stop());
+						break;
+					}
+					case '⏹': {
+						collector.stop();
+						break;
+					}
+					default: break;
+				}
+			})
+
+			collector.on('end', collected => {
+				msg.delete();
+			})
 		})
 	}
 }
