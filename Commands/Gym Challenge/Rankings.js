53a60bbaaa05fc0d8cbbcfc9aa7de1a0a6d4ae2a
Alexander <abertholf7626@floridapoly.edu>
2021-01-13 19:17:03 -0500

    Tab fixing

--- Commands/Gym Challenge/Rankings.js
+++ Commands/Gym Challenge/Rankings.js
@@ -36,49 +36,49 @@ module.exports = {
 			return b.count - a.count;
 		})
     	message.channel.send(await getEmbed(client, badgeArray, index))
-						.then(msg => {
-							let reactions = ["⬅", "➡", "⏹"];
-							reactions.forEach(function(r, i){
-								setTimeout(function(){
-									msg.react(r);
-								}, i*800)
-							})
+		.then(msg => {
+			let reactions = ["⬅", "➡", "⏹"];
+			reactions.forEach(function(r, i){
+				setTimeout(function(){
+					msg.react(r);
+				}, i*800)
+			})
 
-							//set filter to only let only set reactions and message author to respond
-							const filter = (reaction, user) => {
-								return reactions.includes(reaction.emoji.name) && user.id === message.author.id;
-							}
+			//set filter to only let only set reactions and message author to respond
+			const filter = (reaction, user) => {
+				return reactions.includes(reaction.emoji.name) && user.id === message.author.id;
+			}
 
-							//create reactionCollector
-							const collector = msg.createReactionCollector(filter, {});
+			//create reactionCollector
+			const collector = msg.createReactionCollector(filter, {});
 
-							collector.on('collect', async (reaction) => {
-								/*setTimeout(function(){
-									reaction.remove(message.author.id).catch(err => {});
-								}, 250)*/
-								switch(reaction.emoji.name){
-									case '⬅':{
-										index = (index-1) < 0? maxPages-1 :index-1;
-										msg.edit(await getEmbed(client, badgeArray, index));
-										break;
-									}
-									case '➡':{
-										index = (index+1)%maxPages;
-										msg.edit(await getEmbed(client, badgeArray, index));
-										break;
-									}
-									case '⏹':{
-										collector.emit('end');
-										break;
-									}
-								}
-							})
+			collector.on('collect', async (reaction) => {
+				/*setTimeout(function(){
+					reaction.remove(message.author.id).catch(err => {});
+				}, 250)*/
+				switch(reaction.emoji.name){
+					case '⬅':{
+						index = (index-1) < 0? maxPages-1 :index-1;
+						msg.edit(await getEmbed(client, badgeArray, index));
+						break;
+					}
+					case '➡':{
+						index = (index+1)%maxPages;
+						msg.edit(await getEmbed(client, badgeArray, index));
+						break;
+					}
+					case '⏹':{
+						collector.emit('end');
+						break;
+					}
+				}
+			})
 
-							collector.on('end', collected => {
-								msg.delete();
-							})
-						})
-						.catch(err => console.log(err));
+			collector.on('end', collected => {
+				msg.delete();
+			})
+		})
+		.catch(err => console.log(err));
 	}
 }
 
