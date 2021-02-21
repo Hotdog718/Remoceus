257c5134ebd429b3f2beaa34fe73e0c815acb475
Alexander <abertholf7626@floridapoly.edu>
2021-02-21 18:39:55 -0500

    Addded new command to view All GLs/Gym Subs

---
+++ Commands/Gyms/GymLeaders.js
@@ -0,0 +1,51 @@
+const { MessageEmbed, Message, GuildMemberManager } = require('discord.js');
+
+module.exports = {
+	name: "gymleaders",
+	aliases: [],
+	category: "Gyms",
+	description: "Displays the Gym Leaders of each type.",
+	usage: "",
+	permissions: [],
+	run: async (client, message, args) => {
+		await message.guild.members.fetch();
+        await message.guild.roles.fetch();
+
+        const embed = new MessageEmbed()
+        .setTitle(`Gym Leaders of ${message.guild.name}`)
+        .setColor(client.config.color)
+        .setThumbnail(message.guild.iconURL());
+
+		let types = Object.keys(client.config.gymTypes);
+
+        const leaderRole = message.guild.roles.cache.find(r => r.name === 'Gym Leaders');
+        const subRole = message.guild.roles.cache.find(r => r.name === 'Gym Subs');
+        if(leaderRole && subRole){
+            for(const type of types){
+                const gymType = client.config.gymTypes[type];
+                const typeRole = message.guild.roles.cache.find(r => r.name === `${gymType.name} Gym Leader`);
+                if(!typeRole) continue;
+                const gymLeaders = message.guild.members.cache.filter(member => member.roles.cache.has(typeRole.id) && member.roles.cache.has(leaderRole.id));
+                const gymSubs = message.guild.members.cache.filter(member => member.roles.cache.has(typeRole.id) && member.roles.cache.has(subRole.id));
+                const unknown = message.guild.members.cache.filter(member => member.roles.cache.has(typeRole.id) && !(member.roles.cache.has(leaderRole.id) || member.roles.cache.has(subRole.id)));
+                let str = ``;
+                if(gymLeaders && gymLeaders.array().length > 0){
+                    str += `**Gym Leader(s):**\n${gymLeaders.array().join('\n')}\n`;
+                }
+                if(gymSubs && gymSubs.array().length > 0){
+                    str += `**Gym Sub(s):**\n${gymSubs.array().join('\n')}`;
+                }
+                if(unknown && unknown.array().length > 0){
+                    str += `**Unknown:**\n${unknown.array().join('\n')}`;
+                }
+                if(str){
+                    embed.addField(`__${gymType.name}__`, str, true);
+                }
+            }
+        }else{
+            embed.addField("No Gym Leaders/Subs");
+        }
+
+        message.channel.send(embed);
+	}
+}
