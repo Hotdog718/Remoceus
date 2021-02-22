03e0891793cbb360fc116a53894912ff95b98704
Alexander <abertholf7626@floridapoly.edu>
2021-02-22 13:44:01 -0500

    Added cache to a closer look at so I don't anger the database gods

--- Mongo/ACLA.js
+++ Mongo/ACLA.js
@@ -1,7 +1,12 @@
 const mongo = require('./Mongo.js');
 const ACLA = require('./Models/MOTW.js');
 
+const cache = {movesets: [], updated: false} // {"movesets": Array<movesets>, "updated": boolean}
+
 module.exports.getMoveset = async () => {
+    if(cache.updated){
+        return cache.movesets;
+    }
     return await mongo().then(async (mongoose) => {
         try {
             const result = await ACLA.find();
@@ -15,6 +20,9 @@ module.exports.getMoveset = async () => {
 }
 
 module.exports.getMovesetWithSpecies = async (species) => {
+    if(cache.updated){
+        return cache.movesets.filter((val) => val.pokemon === species.toLowerCase());
+    }
     return await mongo().then(async (mongoose) => {
         try {
             const result = await ACLA.find({pokemon: species.toLowerCase()});
@@ -23,6 +31,33 @@ module.exports.getMovesetWithSpecies = async (species) => {
             }
         } finally {
             mongoose.connection.close();
+        }
+    })
+}
+
+module.exports.updateCache = async () => {
+    return await mongo().then(async (mongoose) => {
+        const result = await ACLA.find();
+        if(result){
+            cache.movesets = [];
+            for(const moveset of result){
+                cache.movesets.push({
+                    setName: moveset.setName,
+                    pokemon: moveset.pokemon,
+                    nature: moveset.nature,
+                    ability: moveset.ability,
+                    item: moveset.item,
+                    evs: moveset.evs,
+                    move1: moveset.move1,
+                    move2: moveset.move2,
+                    move3: moveset.move3,
+                    move4: moveset.move4,
+                    forme: moveset.forme,
+                    year: moveset.year,
+                    ytLink: moveset.ytLink
+                });
+            }
+            cache.updated = true;
         }
     })
 }
