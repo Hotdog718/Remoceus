5883edba2943886254877bfce26ce22e09ae98be
Alexander <abertholf7626@floridapoly.edu>
2021-02-23 21:35:18 -0500

    Fixed bug where the wrong ranking changes were given

--- Commands/Judge/TrainerBattle.js
+++ Commands/Judge/TrainerBattle.js
@@ -66,64 +66,25 @@ function tempName(client, message, winnerName, loserName, winnerBadges, loserBad
     serverID: winnerBadges.serverID,
     points: winnerBadges.points + pointsEarned
   }
+  
   let loserTemp = {
     userID: loserBadges.userID,
     serverID: loserBadges.serverID,
     points: Math.max((loserBadges.points - pointsLost), 0)
   }
-  let {rankWinPost, rankLoserPost} = getUpdatedRankings(message, WinnerTemp, loserTemp, badgeArray);
-  if(rankWinPost && rankWin != rankWinPost){
+
+  let rankWinPost = getWinnerRanking(message, WinnerTemp, loserTemp, badgeArray);
+  let rankLoserPost = getLoserRanking(message, WinnerTemp, loserTemp, badgeArray);
+
+  if(rankWinPost && rankWin !== rankWinPost){
     arr.push(`${winnerName} goes from #${rankWin} to #${rankWinPost}`);
   }
 
-  if(rankLoserPost && rankLoser != rankLoserPost){
+  if(rankLoserPost && rankLoser !== rankLoserPost){
     arr.push(`${loserName} goes from #${rankLoser} to #${rankLoserPost}`);
   }
 
   return arr.join('\n');
-}
-
-function getUpdatedRankings(message, user1, user2, arr){
-  const isUser = (user, check) => user.userID == check.userID && user.serverID == check.serverID;
-  const getPoints = (check) => isUser(user1, check) ? user1.points : (isUser(user2, check) ? user2.points : check.points);
-
-  arr.sort((a, b) => {
-    let aPoints = getPoints(a);
-    let bPoints = getPoints(b);
-    if(aPoints > bPoints){
-      return -1;
-    }
-    if(a.points < b.points){
-      return 1;
-    }
-    if(a.count == b.count){
-      let aUser = message.guild.members.cache.get(a.userID);
-      let bUser = message.guild.members.cache.get(b.userID);
-      let aName = aUser ? aUser.user.username : "User not found";
-      let bName = bUser ? bUser.user.username : "User not found";
-      if(aName > bName){
-        return 1;
-      }
-      if(aName < bName){
-        return -1;
-      }
-      return 0;
-    }
-    return b.count - a.count;
-  })
-
-  let ranking = 1;
-  let obj = {};
-  for(let i = 0; i < arr.length; i++){
-    if(i > 0 && getPoints(arr[i-1]) > getPoints(arr[i])) ranking++;
-    if(user1.userID == arr[i].userID && user1.serverID == arr[i].serverID){
-      obj.rankWinPost = ranking;
-    }
-    if(user2.userID == arr[i].userID && user2.serverID == arr[i].serverID){
-      obj.rankLoserPost = ranking;
-    }
-  }
-  return obj;
 }
 
 function winnerPoints(client, winner, loser, rankWin, rankLoser){
@@ -143,4 +104,66 @@ function loserPoints(winner, loser){
   }else{
     return Math.ceil(5 + (PD / 8));
   }
+}
+
+function getWinnerRanking(message, winnerPoints, loserPoints, pointsArray){
+  const isWinner = (user) => winnerPoints.userID == user.userID && winnerPoints.serverID == user.serverID;
+  const isLoser = (user) => loserPoints.userID == user.userID && loserPoints.serverID == user.serverID;
+      
+  const badgeArray = pointsArray.filter(value => {
+    const member = message.guild.members.cache.get(value.userID);
+    if(!member) return false;
+    return true;
+  })
+  .sort((a, b) => {
+    let aPoints = (isWinner(a) ? winnerPoints.points : (isLoser(a) ? loserPoints.points : a.points));
+    let bPoints = (isWinner(b) ? winnerPoints.points : (isLoser(b) ? loserPoints.points : b.points));
+    return bPoints - aPoints;
+  })
+
+  let ranking = 1;
+  for(let i = 0; i < badgeArray.length; i++){
+    if(i > 0){
+      let a = badgeArray[i];
+      let b = badgeArray[i-1];
+
+      let aPoints = (isWinner(a) ? winnerPoints.points : (isLoser(a) ? loserPoints.points : a.points));
+      let bPoints = (isWinner(b) ? winnerPoints.points : (isLoser(b) ? loserPoints.points : b.points));
+
+      if(bPoints > aPoints) ranking++;
+    }
+    if(winnerPoints.userID === badgeArray[i].userID && winnerPoints.serverID === badgeArray[i].serverID) break;
+  }
+  return ranking;
+}
+
+function getLoserRanking(message, winnerPoints, loserPoints, pointsArray){
+  const isWinner = (user) => winnerPoints.userID == user.userID && winnerPoints.serverID == user.serverID;
+  const isLoser = (user) => loserPoints.userID == user.userID && loserPoints.serverID == user.serverID;
+      
+  const badgeArray = pointsArray.filter(value => {
+    const member = message.guild.members.cache.get(value.userID);
+    if(!member) return false;
+    return true;
+  })
+  .sort((a, b) => {
+    let aPoints = (isWinner(a) ? winnerPoints.points : (isLoser(a) ? loserPoints.points : a.points));
+    let bPoints = (isWinner(b) ? winnerPoints.points : (isLoser(b) ? loserPoints.points : b.points));
+    return bPoints - aPoints;
+  });
+
+  let ranking = 1;
+  for(let i = 0; i < badgeArray.length; i++){
+    if(i > 0){
+      let a = badgeArray[i];
+      let b = badgeArray[i-1];
+
+      let aPoints = (isWinner(a) ? winnerPoints.points : (isLoser(a) ? loserPoints.points : a.points));
+      let bPoints = (isWinner(b) ? winnerPoints.points : (isLoser(b) ? loserPoints.points : b.points));
+
+      if(bPoints > aPoints) ranking++;
+    }
+    if(loserPoints.userID === badgeArray[i].userID && loserPoints.serverID === badgeArray[i].serverID) break;
+  }
+  return ranking;
 }
